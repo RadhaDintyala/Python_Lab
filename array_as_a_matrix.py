e9a952df8457a3f92698a3dfd362ddad6a391061
Radha kalyani <199371027+RadhaDintyala@users.noreply.github.com>
2025-11-03 21:28:44 +0530

    Refactor matrix operations for clarity ,variable changed

--- array_as_a_matrix.py
+++ array_as_a_matrix.py
@@ -8,8 +8,8 @@ def mat_product(mat1,mat2):
         for j in range(2):
             result[i][j]=(mat1[i][0]*mat2[0][j]+mat1[i][1]*mat2[1][j])
     print("Multiplication:")
-    for row in result:
-        print(row)
+    for k in result:
+        print(k)
 mat_product(mat1,mat2)
 
 #Addition 
@@ -18,8 +18,8 @@ def mat_add(mat1,mat2):
         for j in range(2):
             result[i][j]=mat1[i][j]+mat2[i][j]
     print("addition:")
-    for row in result:
-        print(row)
+    for k in result:
+        print(k)
 mat_add(mat1,mat2)
 
 #Transpose
@@ -28,6 +28,7 @@ def mat_transpose(mat1):
         for j in range(2):
             transpose[i][j]=mat1[j][i]
     print("Transpose:")
-    for row in transpose:
-            print(row)
+    for k in transpose:
+            print(k)
+
 mat_transpose(mat1)
